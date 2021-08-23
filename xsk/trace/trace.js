e8d63909ade1698e409e68b66f7cfb60ef4b63a8
Nedelcho Delchev <delchevn@gmail.com>
2021-08-23 11:21:23 +0300

    license headers update

--- xsk/trace/trace.js
+++ xsk/trace/trace.js
@@ -1,50 +1,61 @@
 /*
- * Copyright (c) 2019-2020 SAP SE or an SAP affiliate company and XSK contributors
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License, v2.0
  * which accompanies this distribution, and is available at
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * SPDX-FileCopyrightText: 2019-2020 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
  * SPDX-License-Identifier: Apache-2.0
  */
 exports.debug = function(message) {
-	console.debug(message);
+  if(this.isDebugEnabled()) {
+    console.debug(message);
+  }
 }
 
 exports.error = function(message) {
-	console.error(message);
+  if(this.isErrorEnabled()) {
+    console.error(message);
+  }
 }
 
 exports.fatal = function(message) {
-	console.error(message);
+  if(this.isFatalEnabled()) {
+    console.error(message);
+  }
 }
 
 exports.info = function(message) {
-	console.info(message);
+  if(this.isInfoEnabled()) {
+    console.info(message);
+  }
 }
 
 exports.warning = function(message) {
-	console.warn(message);
+  if(this.isWarningEnabled()) {
+    console.warn(message);
+  }
 }
 
 exports.isDebugEnabled = function() {
-	return true;
+	return com.sap.xsk.api.TraceFacade.isDebugEnabled();
 }
 
 exports.isErrorEnabled = function() {
-	return true;
+  return com.sap.xsk.api.TraceFacade.isErrorEnabled();
 }
 
 exports.isFatalEnabled = function() {
-	return true;
+  return com.sap.xsk.api.TraceFacade.isFatalEnabled();
 }
 
 exports.isInfoEnabled = function() {
-	return true;
+  return com.sap.xsk.api.TraceFacade.isInfoEnabled();
 }
 
 exports.isWarningEnabled = function() {
-	return true;
+  return com.sap.xsk.api.TraceFacade.isWarningEnabled();
 }
+
