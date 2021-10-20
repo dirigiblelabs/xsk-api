f1e28b2fc17c9ede06eb2bd9e38e09e1e96461de
Ayhan Rashidov <36267628+aihanrashidov@users.noreply.github.com>
2021-10-20 12:46:22 +0300

    Update api.js

--- xsk/api.js
+++ xsk/api.js
@@ -20,17 +20,18 @@ $.hdb = require('xsk/hdb/hdb');
 $.net = require('xsk/net/net');
 $.import = require("xsk/import/import").import;
 $.trace = require('xsk/trace/trace');
-$.security = require('xsk/security/security');
 $.util = require('xsk/util/util');
+$.jobs = require('xsk/jobs/jobs');
 
 try {
-    let dResponse = require('xsk/web/web').WebResponse;
-    let dRequest = require('xsk/web/web').WebRequest;
-    $.session = require('xsk/session/session');
-    $.request = new dRequest();
-    $.response = new dResponse();
+  let dResponse = require('xsk/web/web').WebResponse;
+  let dRequest = require('xsk/web/web').WebRequest;
+  $.session = require('xsk/session/session');
+  $.request = new dRequest();
+  $.response = new dResponse();
+  $.security = require('xsk/security/security');
 } catch (e) {
-    // $.trace .warning("Caught exception. Api.js is being used by xsk job.")
+  // $.trace.warning("Caught exception. Api.js is being used by xsk job.")
 }
 
 $;
