b82b4327d76ce9d886c54c1d8a6241bdc794c203
c5326374 <iwolkkov@gmail.com>
2022-07-18 09:20:30 +0300

    - refactor crypto to be called as property of security - make crypto methods to accept and return JS ArrayBuffer instead of Java byte[]

--- xsk/security/security.js
+++ xsk/security/security.js
@@ -12,55 +12,79 @@
 var repositoryManager = require("platform/v4/repository");
 
 exports.Store = function (filePath) {
-    var REGISTRY_DIR_PREFIX = "/registry/public/";
-    filePath = REGISTRY_DIR_PREFIX + filePath;
+	var REGISTRY_DIR_PREFIX = "/registry/public/";
+	filePath = REGISTRY_DIR_PREFIX + filePath;
 
-    repositoryManager.getResource(filePath);
-    var existsStore = com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.existsStore(filePath);
-    if (!existsStore) {
-        throw new Error("Not such secure store found.")
-    }
+	repositoryManager.getResource(filePath);
+	var existsStore = com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.existsStore(filePath);
+	if (!existsStore) {
+		throw new Error("Not such secure store found.")
+	}
 
-    this.store = function(writeObject) {
-        com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.store(filePath, writeObject.name, writeObject.value);
-    }
+	this.store = function (writeObject) {
+		com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.store(filePath, writeObject.name, writeObject.value);
+	}
 
-    this.storeForUser = function(writeObject) {
-        com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.storeForUser(filePath, writeObject.name, writeObject.value);
-    }
+	this.storeForUser = function (writeObject) {
+		com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.storeForUser(filePath, writeObject.name, writeObject.value);
+	}
 
-    this.readForUser = function(readObject) {
-        return com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.readForUser(filePath, readObject.name);
-    }
+	this.readForUser = function (readObject) {
+		return com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.readForUser(filePath, readObject.name);
+	}
 
-    this.read = function(readObject) {
-        return com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.read(filePath, readObject.name);
-    }
+	this.read = function (readObject) {
+		return com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.read(filePath, readObject.name);
+	}
 
-    this.remove = function(removeObject) {
-        com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.remove(filePath, removeObject.name);
-    }
+	this.remove = function (removeObject) {
+		com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.remove(filePath, removeObject.name);
+	}
 
-    this.removeForUser = function(removeObject) {
-        com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.removeForUser(filePath, removeObject.name);
-    }
+	this.removeForUser = function (removeObject) {
+		com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.removeForUser(filePath, removeObject.name);
+	}
 }
 
-exports.crypto = function (){
-    return new XSCrypto()
-
-}
 
 class XSCrypto {
-    CryptoFacade = Java.type("com.sap.xsk.xssecurestore.ds.facade.XSKSecureCryptoFacade");
+	CryptoFacade = Java.type("com.sap.xsk.xssecurestore.ds.facade.XSKSecureCryptoFacade");
 
-    md5(data,key){
-        return this.CryptoFacade.md5(data,key);
-    }
-    sha1(data,key){
-        return this.CryptoFacade.sha1(data,key);
-    }
-    sha256(data,key){
-        return this.CryptoFacade.sha256(data,key);
-    }
+	md5(data, key) {
+		if (typeof data == 'object') {
+			data = fromBufferToArray(data);
+		}
+		const javaBytes = this.CryptoFacade.md5(data, key);
+		return fromArrayToBuffer(javaBytes);
+	}
+
+	sha1(data, key) {
+		if (typeof data == 'object') {
+			data = fromBufferToArray(data);
+		}
+		const javaBytes = this.CryptoFacade.sha1(data, key);
+		return fromArrayToBuffer(javaBytes);
+	}
+
+	sha256(data, key) {
+		if (typeof data == 'object') {
+			data = fromBufferToArray(data);
+		}
+		const javaBytes = this.CryptoFacade.sha256(data, key);
+		return fromArrayToBuffer(javaBytes);
+	}
+}
+
+exports.crypto = new XSCrypto;
+
+// From ArrayBuffer to byte[]
+function fromBufferToArray(buffer) {
+	return Array.from(new Uint8Array(buffer))
+}
+
+// from Java byte[]  to JS ArrayBuffer
+function fromArrayToBuffer(javaBytes) {
+	var uint8Array = new Uint8Array(javaBytes.length);
+	uint8Array.set(Java.from(javaBytes));
+	return uint8Array.buffer
 }
