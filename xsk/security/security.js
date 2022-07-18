29bf8effa29cc13325b150aa2633df4914938dee
Ivan Volkov <61655573+ivanvolkoff@users.noreply.github.com>
2022-07-18 16:15:16 +0300

    changes

--- xsk/security/security.js
+++ xsk/security/security.js
@@ -12,38 +12,38 @@
 var repositoryManager = require("platform/v4/repository");
 
 exports.Store = function (filePath) {
-	var REGISTRY_DIR_PREFIX = "/registry/public/";
-	filePath = REGISTRY_DIR_PREFIX + filePath;
+    var REGISTRY_DIR_PREFIX = "/registry/public/";
+    filePath = REGISTRY_DIR_PREFIX + filePath;
 
-	repositoryManager.getResource(filePath);
-	var existsStore = com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.existsStore(filePath);
-	if (!existsStore) {
-		throw new Error("Not such secure store found.")
-	}
+    repositoryManager.getResource(filePath);
+    var existsStore = com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.existsStore(filePath);
+    if (!existsStore) {
+        throw new Error("Not such secure store found.")
+    }
 
-	this.store = function (writeObject) {
-		com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.store(filePath, writeObject.name, writeObject.value);
-	}
+    this.store = function(writeObject) {
+        com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.store(filePath, writeObject.name, writeObject.value);
+    }
 
-	this.storeForUser = function (writeObject) {
-		com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.storeForUser(filePath, writeObject.name, writeObject.value);
-	}
+    this.storeForUser = function(writeObject) {
+        com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.storeForUser(filePath, writeObject.name, writeObject.value);
+    }
 
-	this.readForUser = function (readObject) {
-		return com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.readForUser(filePath, readObject.name);
-	}
+    this.readForUser = function(readObject) {
+        return com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.readForUser(filePath, readObject.name);
+    }
 
-	this.read = function (readObject) {
-		return com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.read(filePath, readObject.name);
-	}
+    this.read = function(readObject) {
+        return com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.read(filePath, readObject.name);
+    }
 
-	this.remove = function (removeObject) {
-		com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.remove(filePath, removeObject.name);
-	}
+    this.remove = function(removeObject) {
+        com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.remove(filePath, removeObject.name);
+    }
 
-	this.removeForUser = function (removeObject) {
-		com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.removeForUser(filePath, removeObject.name);
-	}
+    this.removeForUser = function(removeObject) {
+        com.sap.xsk.xssecurestore.ds.facade.XSKSecureStoreFacade.removeForUser(filePath, removeObject.name);
+    }
 }
 
 
@@ -51,7 +51,7 @@ class XSCrypto {
 	CryptoFacade = Java.type("com.sap.xsk.xssecurestore.ds.facade.XSKSecureCryptoFacade");
 
 	md5(data, key) {
-		if (typeof data == 'object') {
+		if (data instanceof ArrayBuffer) {
 			data = fromBufferToArray(data);
 		}
 		const javaBytes = this.CryptoFacade.md5(data, key);
@@ -59,7 +59,7 @@ class XSCrypto {
 	}
 
 	sha1(data, key) {
-		if (typeof data == 'object') {
+		if (data instanceof ArrayBuffer) {
 			data = fromBufferToArray(data);
 		}
 		const javaBytes = this.CryptoFacade.sha1(data, key);
@@ -67,7 +67,7 @@ class XSCrypto {
 	}
 
 	sha256(data, key) {
-		if (typeof data == 'object') {
+		if (data instanceof ArrayBuffer) {
 			data = fromBufferToArray(data);
 		}
 		const javaBytes = this.CryptoFacade.sha256(data, key);
@@ -75,7 +75,7 @@ class XSCrypto {
 	}
 }
 
-exports.crypto = new XSCrypto;
+exports.crypto = new XSCrypto();
 
 // From ArrayBuffer to byte[]
 function fromBufferToArray(buffer) {
